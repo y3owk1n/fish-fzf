ddd3c353f9cebd5bde15379a1c297f6c145b8851
y3owk1n <wongyeowkin@gmail.com>
2024-01-06 00:34:39 +0800

    chore: refactor command history completion

--- completions/_fzf_cmd_history.fish
+++ completions/_fzf_cmd_history.fish
@@ -1,6 +1,4 @@
-# ~/.config/fish/completions/_fzf_cmd_history.fish
-
-function __fzf_cmd_history_completion
+function _fzf_cmd_history_completion
     set -l cmd_history_options '--prompt-name --allow-execute'
 
     for opt in $cmd_history_options
@@ -13,4 +11,4 @@ function __fzf_cmd_history_completion
     echo $cmd_history_options
 end
 
-complete -c _fzf_cmd_history -f -n __fzf_cmd_history_completion -d 'fzf command history'
+complete -c _fzf_cmd_history -f -n _fzf_cmd_history_completion -d 'fzf command history'
