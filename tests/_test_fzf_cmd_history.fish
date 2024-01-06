7abda311fcf7ceddfcd64d3e5905641f583976c0
y3owk1n <wongyeowkin@gmail.com>
2024-01-06 19:16:01 +0800

    chore: fixing commandline not available in CI environment

--- tests/_test_fzf_cmd_history.fish
+++ tests/_test_fzf_cmd_history.fish
@@ -18,16 +18,24 @@ function setup_mocked_fzf
     end
 end
 
+# Variable to capture the output of _fzf_cmd_history
+set selected_command_output
+
+# Function to override echo and capture the output
+function echo -e
+    set selected_command_output $argv[1]
+end
+
 # Test _fzf_cmd_history without options
 setup_mocked_history
 set selected_command (echo "command1" | _fzf_cmd_history)
-set selected_command_without_equals (string replace '=' '' $selected_command)
+set selected_command_without_equals (string replace '=' '' $selected_command_output)
 @test "selected command is correct" "$selected_command_without_equals" = command1
 
 # Test _fzf_cmd_history with custom prompt name
 setup_mocked_history
 set selected_command (echo "command2" | _fzf_cmd_history --prompt-name "CustomPrompt")
-set selected_command_without_equals (string replace '=' '' $selected_command)
+set selected_command_without_equals (string replace '=' '' $selected_command_output)
 @test "selected command is correct with custom prompt name" "$selected_command_without_equals" = command2
 @test "prompt name in fzf is correct" $FZF_PROMPT_NAME = CustomPrompt
 
@@ -35,7 +43,7 @@ set selected_command_without_equals (string replace '=' '' $selected_command)
 setup_mocked_history
 setup_mocked_fzf
 set selected_command (echo "command3" | _fzf_cmd_history --allow-execute)
-set selected_command_without_equals (string replace '=' '' $selected_command)
+set selected_command_without_equals (string replace '=' '' $selected_command_output)
 @test "selected command is correct with allow-execute option" "$selected_command_without_equals" = command3
 @test "prompt name in fzf is correct" $FZF_PROMPT_NAME = "Command History"
 
@@ -43,7 +51,7 @@ set selected_command_without_equals (string replace '=' '' $selected_command)
 setup_mocked_history
 setup_mocked_fzf
 set selected_command (echo "command4" | _fzf_cmd_history --prompt-name "CustomPrompt" --allow-execute)
-set selected_command_without_equals (string replace '=' '' $selected_command)
+set selected_command_without_equals (string replace '=' '' $selected_command_output)
 @test "selected command is correct with both options" "$selected_command_without_equals" = command4
 @test "prompt name in fzf is correct" $FZF_PROMPT_NAME = CustomPrompt
 
